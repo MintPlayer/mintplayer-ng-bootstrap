532e568b08214fb52aee68c33e1a65988fb21268
DESKTOP-K4FBQRK\piete <pieterjandeclippel@msn.com>
2021-08-07 16:06:29 +0200

    initial commit

---
+++ apps/ng-bootstrap-demo/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mintplayer-ng-bootstrap-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'ng-bootstrap-demo';
+}
