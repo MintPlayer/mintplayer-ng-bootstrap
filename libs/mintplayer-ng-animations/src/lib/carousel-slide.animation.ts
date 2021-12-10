24178123f50163b7ed0dc10b24d1377bbc386bb8
DESKTOP-J4N5ARE\Pieterjan De Clippel <pieterjan@2sky.be>
2021-12-10 15:57:52 +0100

    Temp commit

--- libs/mintplayer-ng-animations/src/lib/carousel-slide.animation.ts
+++ libs/mintplayer-ng-animations/src/lib/carousel-slide.animation.ts
@@ -1,13 +1,65 @@
-import { trigger, state, style, transition, group, animate } from '@angular/animations';
+import { trigger, state, style, transition, group, animate, query } from '@angular/animations';
 
 export const CarouselSlideAnimation =
   trigger('carouselSlide', [
-    transition(':enter', [
-      style({ left: '100%' }),
-      animate('400ms', style({ left: 0 })),
+    // One time initial load. Move page from left -100% to 0%
+    transition('-1 => *', [
+      query(':enter', [
+        style({ position: 'fixed', width: '100%', transform: 'translateX(-100%)' }),
+        animate(
+          '50000ms ease',
+          style({ opacity: 1, transform: 'translateX(0%)' }),
+        ),
+      ]),
     ]),
-    transition(':leave', [
-      style({ left: '0' }),
-      animate('400ms', style({ left: '-100%' }))
+
+    // Previous, slide left to right to show left page
+    transition(':decrement', [
+      // set new page X location to be -100%
+      query(':enter',
+        style({ position: 'fixed', width: '100%', transform: 'translateX(-100%)' }),
+      ),
+
+      group([
+        // slide existing page from 0% to 100% to the right
+        query(':leave',
+          animate(
+            '50000ms ease',
+            style({ position: 'fixed', width: '100%', transform: 'translateX(100%)' }),
+          ),
+        ),
+        // slide new page from -100% to 0% to the right
+        query(':enter',
+          animate(
+            '50000ms ease',
+            style({ opacity: 1, transform: 'translateX(0%)' }),
+          ),
+        ),
+      ]),
     ]),
+
+    // Next, slide right to left to show right page
+    transition(':increment', [
+      // set new page X location to be 100%
+      query(':enter',
+        style({ position: 'fixed', width: '100%', transform: 'translateX(100%)' }),
+      ),
+
+      group([
+        // slide existing page from 0% to -100% to the left
+        query(':leave',
+          animate(
+            '50000ms ease',
+            style({ position: 'fixed', width: '100%', transform: 'translateX(-100%)' }),
+          ),
+        ),
+        // slide new page from 100% to 0% to the left
+        query(':enter',
+          animate(
+            '50000ms ease',
+            style({ opacity: 1, transform: 'translateX(0%)' }),
+          ),
+        ),
+      ]),
+    ])
   ]);
--- libs/mintplayer-ng-bootstrap/src/lib/components/carousel/carousel/carousel.component.ts
+++ libs/mintplayer-ng-bootstrap/src/lib/components/carousel/carousel/carousel.component.ts
@@ -1,5 +1,7 @@
-import { AfterContentInit, Component, ContentChildren, ElementRef, OnInit, QueryList } from '@angular/core';
+import { AfterContentInit, Component, ContentChildren, ElementRef, OnDestroy, OnInit, QueryList } from '@angular/core';
 import { FadeInOutAnimation, CarouselSlideAnimation } from '@mintplayer/ng-animations';
+import { BehaviorSubject, Observable, Subject } from 'rxjs';
+import { map, take, takeUntil } from 'rxjs/operators';
 import { BsCarouselImageDirective } from '../carousel-image/carousel-image.directive';
 
 @Component({
@@ -8,76 +10,60 @@ import { BsCarouselImageDirective } from '../carousel-image/carousel-image.direc
   styleUrls: ['./carousel.component.scss'],
   animations: [FadeInOutAnimation, CarouselSlideAnimation]
 })
-export class BsCarouselComponent implements OnInit, AfterContentInit {
+export class BsCarouselComponent implements OnInit, OnDestroy, AfterContentInit {
 
-  constructor() { }
+  constructor() {
+    this.currentImage$ = this.currentImageIndex$
+      .pipe(map((index) => this.images.get(index) ?? null))
+      .pipe(takeUntil(this.destroyed$));
+  }
 
-  ngOnInit(): void {
+  ngOnInit(): void { }
+
+  ngOnDestroy() {
+    this.destroyed$.next(true);
   }
 
   ngAfterContentInit() {
-    const first = this.images.get(0);
-    if (!!first) {
-      this.startAnimation(first.nativeElement);
+    if (this.images.length > 0) {
+      this.currentImageIndex$.next(0);
     } else {
-      this.startAnimation(null);
+      this.currentImageIndex$.next(-1);
     }
   }
 
+  destroyed$ = new Subject();
+  currentImageIndex$ = new BehaviorSubject<number>(-1);
+  currentImage$: Observable<ElementRef<HTMLImageElement> | null>;
+
   previousImage() {
-    const currentIndex = this.images.map(i => i.nativeElement).findIndex(i => i === this.currentImage);
-    if (currentIndex === 0) {
-      this.startAnimation(this.images.last.nativeElement);
-    } else {
-      const prev = this.images.get(currentIndex - 1);
-      if (!prev) {
-      this.startAnimation(null);
-      } else {
-        this.startAnimation(prev.nativeElement);
-      }
-    }
+    this.currentImageIndex$
+      .pipe(take(1))
+      .subscribe((currentImageIndex) => {
+        if (currentImageIndex === 0) {
+          this.currentImageIndex$.next(this.images.length - 1);
+        } else {
+          this.currentImageIndex$.next(currentImageIndex - 1);
+        }
+      });
   }
 
   nextImage() {
-    const currentIndex = this.images.map(i => i.nativeElement).findIndex(im => im === this.currentImage);
-    if (currentIndex >= this.images.length - 1) {
-        this.startAnimation(this.images.first.nativeElement);
-    } else {
-      const next = this.images.get(currentIndex + 1);
-      if (!next) {
-        this.startAnimation(null);
-      } else {
-        this.startAnimation(next.nativeElement);
-      }
-    }
+    this.currentImageIndex$
+      .pipe(take(1))
+      .subscribe((currentImageIndex) => {
+        if (currentImageIndex >= this.images.length - 1) {
+          this.currentImageIndex$.next(0);
+        } else {
+          this.currentImageIndex$.next(currentImageIndex + 1);
+        }
+      });
   }
 
   setCurrentImage(index: number) {
-    const image = this.images.get(index);
-    if (!image) {
-      this.startAnimation(this.images.get(0)?.nativeElement ?? null);
-    } else {
-      this.startAnimation(image.nativeElement);
-    }
-  }
-
-  startAnimation(image: HTMLImageElement | null) {
-    const previousImage = this.currentImage;
-    this.currentImage = image;
-
-    if (image) {
-      this.shownImages.push(image);
-    }
-
-    // setTimeout(() => {
-      if (previousImage) {
-        this.shownImages.splice(this.shownImages.indexOf(previousImage), 1);
-      }
-    // }, 1000);
+    this.currentImageIndex$.next(index);
   }
 
   @ContentChildren(BsCarouselImageDirective, { read: ElementRef }) images!: QueryList<ElementRef<HTMLImageElement>>;
-  currentImage: HTMLImageElement | null = null;
-  shownImages: HTMLImageElement[] = [];
 
 }
