cf7d5cdc266e12aa04d7ed87cec5bfc00f7ed024
DESKTOP-J4N5ARE\Pieterjan De Clippel <pieterjan@2sky.be>
2021-12-14 09:20:31 +0100

    Resolve circular dependency, use version 13.0.4

--- libs/mintplayer-ng-bootstrap/src/lib/components/alert/alert-close/alert-close.component.ts
+++ libs/mintplayer-ng-bootstrap/src/lib/components/alert/alert-close/alert-close.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { BsAlertComponent } from '@mintplayer/ng-bootstrap';
+import { BsAlertComponent } from '../alert/alert.component';
 
 @Component({
   selector: 'bs-alert-close',
