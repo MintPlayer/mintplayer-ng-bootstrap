46b455b0a9728c51535a4e220312c8dcd4aa4a2d
DESKTOP-J4N5ARE\Pieterjan De Clippel <pieterjan@2sky.be>
2021-12-29 12:26:48 +0100

    Expose RowTemplateDirective

--- libs/mintplayer-ng-bootstrap/src/lib/components/datatable/index.ts
+++ libs/mintplayer-ng-bootstrap/src/lib/components/datatable/index.ts
@@ -1,4 +1,5 @@
 export * from './datatable.module';
 export * from './datatable/datatable.component';
+export * from './row-template/row-template.directive';
 export * from './datatable-column/datatable-column.directive';
 export * from './datatable-settings';
