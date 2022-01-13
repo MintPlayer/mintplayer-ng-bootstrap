e219a8436770f3d6a00c9ab5fa39b60babc268f2
DESKTOP-J4N5ARE\Pieterjan De Clippel <pieterjan@2sky.be>
2022-01-13 22:55:11 +0100

    expose button-template

--- libs/mintplayer-ng-bootstrap/src/lib/components/multiselect/directives/index.ts
+++ libs/mintplayer-ng-bootstrap/src/lib/components/multiselect/directives/index.ts
@@ -1,2 +1,3 @@
 export * from './header-template/header-template.directive';
 export * from './footer-template/footer-template.directive';
+export * from './button-template/button-template.directive';
