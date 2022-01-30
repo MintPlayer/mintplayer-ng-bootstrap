ec3ea2f4da0a8722711bed826d92ee8663812503
DESKTOP-J4N5ARE\Pieterjan De Clippel <pieterjan@2sky.be>
2022-01-30 15:21:20 +0100

    Check isPlatformServer for ResizeObserver

--- libs/mintplayer-ng-bootstrap/src/lib/components/navbar/navbar-content/navbar-content.directive.ts
+++ libs/mintplayer-ng-bootstrap/src/lib/components/navbar/navbar-content/navbar-content.directive.ts
@@ -1,4 +1,5 @@
-import { AfterViewInit, Directive, ElementRef, Input, OnDestroy } from '@angular/core';
+import { isPlatformServer } from '@angular/common';
+import { AfterViewInit, Directive, ElementRef, Inject, Input, OnDestroy, PLATFORM_ID } from '@angular/core';
 import { BsNavbarComponent } from '../navbar/navbar.component';
 
 @Directive({
@@ -6,25 +7,33 @@ import { BsNavbarComponent } from '../navbar/navbar.component';
 })
 export class NavbarContentDirective implements AfterViewInit, OnDestroy {
 
-  constructor(private element: ElementRef) {
-    this.resizeObserver = new ResizeObserver((entries) => {
-      let height = entries[0].contentRect.height;
-      this.element.nativeElement.style.paddingTop = (this.initialPadding + height) + 'px';
-    });
+  constructor(private element: ElementRef, @Inject(PLATFORM_ID) private platformId: any) {
+    if (!isPlatformServer(platformId)) {
+      this.resizeObserver = new ResizeObserver((entries) => {
+        const height = entries[0].contentRect.height;
+        this.element.nativeElement.style.paddingTop = (this.initialPadding + height) + 'px';
+      });
+    } else {
+      this.resizeObserver = null;
+    }
   }
+
+  resizeObserver: ResizeObserver | null;
+  initialPadding = 0;
 
   @Input('navbarContent') navbar!: BsNavbarComponent;
 
   ngAfterViewInit() {
-    let p = parseInt(this.element.nativeElement.style.paddingTop.replace(/px$/, ''));
-    this.initialPadding = isNaN(p) ? 0 : p;
-    this.resizeObserver.observe(this.navbar.nav.nativeElement);
+    const pt = parseInt(this.element.nativeElement.style.paddingTop.replace(/px$/, ''));
+    this.initialPadding = isNaN(pt) ? 0 : pt;
+    if (this.resizeObserver) {
+      this.resizeObserver.observe(this.navbar.nav.nativeElement);
+    }
   }
 
   ngOnDestroy() {
-    this.resizeObserver.unobserve(this.navbar.nav.nativeElement);
+    if (this.resizeObserver) {
+      this.resizeObserver.unobserve(this.navbar.nav.nativeElement);
+    }
   }
-
-  resizeObserver: ResizeObserver;
-  initialPadding: number = 0;
 }
