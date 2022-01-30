ad3525fdabee2d9e3247fd71666d209859c864f3
DESKTOP-J4N5ARE\Pieterjan De Clippel <pieterjan@2sky.be>
2022-01-30 17:22:20 +0100

    navbar - Only start observing afterviewinit and navbarContent set

--- libs/mintplayer-ng-bootstrap/src/lib/components/navbar/navbar-content/navbar-content.directive.ts
+++ libs/mintplayer-ng-bootstrap/src/lib/components/navbar/navbar-content/navbar-content.directive.ts
@@ -1,5 +1,6 @@
 import { isPlatformServer } from '@angular/common';
 import { AfterViewInit, Directive, ElementRef, Inject, Input, OnDestroy, PLATFORM_ID } from '@angular/core';
+import { BehaviorSubject, combineLatest, filter, Subject, take, takeUntil } from 'rxjs';
 import { BsNavbarComponent } from '../navbar/navbar.component';
 
 @Directive({
@@ -8,32 +9,51 @@ import { BsNavbarComponent } from '../navbar/navbar.component';
 export class NavbarContentDirective implements AfterViewInit, OnDestroy {
 
   constructor(private element: ElementRef, @Inject(PLATFORM_ID) private platformId: any) {
-    if (!isPlatformServer(platformId)) {
-      this.resizeObserver = new ResizeObserver((entries) => {
-        const height = entries[0].contentRect.height;
-        this.element.nativeElement.style.paddingTop = (this.initialPadding + height) + 'px';
+    combineLatest([this.viewInit$, this.navbar$])
+      .pipe(filter(([viewInit, navbar]) => {
+        return viewInit && !!navbar;
+      }))
+      .pipe(take(1))
+      .pipe(takeUntil(this.destroyed$))
+      .subscribe(([viewInit, navbar]) => {
+        if (!isPlatformServer(platformId)) {
+          // Initialize the ResizeObserver
+          this.resizeObserver = new ResizeObserver((entries) => {
+            const height = entries[0].contentRect.height;
+            this.element.nativeElement.style.paddingTop = (this.initialPadding + height) + 'px';
+          });
+
+          // Monitor the size
+          const pt = parseInt(this.element.nativeElement.style.paddingTop.replace(/px$/, ''));
+          this.initialPadding = isNaN(pt) ? 0 : pt;
+          if (this.resizeObserver && navbar) {
+            this.resizeObserver.observe(navbar.nav.nativeElement);
+          }
+        }
       });
-    } else {
-      this.resizeObserver = null;
-    }
+
+    this.destroyed$
+      .pipe(filter(d => !!d))
+      .subscribe(() => {
+        this.resizeObserver?.unobserve(this.navbar$.value?.nav.nativeElement);
+      });
   }
 
-  resizeObserver: ResizeObserver | null;
+  private destroyed$ = new Subject();
+  private viewInit$ = new BehaviorSubject<boolean>(false);
+  private navbar$ = new BehaviorSubject<BsNavbarComponent | null>(null);
+  resizeObserver: ResizeObserver | null = null;
   initialPadding = 0;
 
-  @Input('navbarContent') navbar!: BsNavbarComponent;
-
+  @Input('navbarContent') set navbar(value: BsNavbarComponent) {
+    this.navbar$.next(value);
+  }
+  
   ngAfterViewInit() {
-    const pt = parseInt(this.element.nativeElement.style.paddingTop.replace(/px$/, ''));
-    this.initialPadding = isNaN(pt) ? 0 : pt;
-    if (this.resizeObserver) {
-      this.resizeObserver.observe(this.navbar.nav.nativeElement);
-    }
+    this.viewInit$.next(true);
   }
 
   ngOnDestroy() {
-    if (this.resizeObserver) {
-      this.resizeObserver.unobserve(this.navbar.nav.nativeElement);
-    }
+    this.destroyed$.next(true);
   }
 }
