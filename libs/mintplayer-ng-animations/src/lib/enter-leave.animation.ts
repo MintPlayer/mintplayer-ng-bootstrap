8696ff93cfedc0268d23731d7f01d6a174044d6d
Pieterjan De Clippel <pieterjandeclippel@msn.com>
2022-05-25 23:31:46 +0200

    Add enter-leave animation

---
+++ libs/mintplayer-ng-animations/src/lib/enter-leave.animation.ts
@@ -0,0 +1,24 @@
+import { trigger, style, transition, animate } from '@angular/animations';
+
+export const EnterLeaveAnimation =
+  trigger('enterLeave', [
+    transition(
+      ':enter', [
+        style({ top: '-50%' }),
+        animate('{{ duration }} ease-in-out', style({ top: 0 })),
+      ], {
+        params: {
+          duration: '500ms'
+        }
+      }
+    ),
+    transition(
+      ':leave', [
+        animate('{{ duration }} ease-in-out', style({ top: '-50%' }))
+      ], {
+        params: {
+          duration: '500ms'
+        }
+      }
+    ),
+  ]);
