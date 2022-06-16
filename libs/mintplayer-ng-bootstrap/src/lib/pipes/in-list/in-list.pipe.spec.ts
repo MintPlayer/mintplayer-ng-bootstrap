3828d28fe19ad1b588325c1c7914d4941f8ecf2f
Pieterjan De Clippel <pieterjandeclippel@msn.com>
2022-06-16 10:02:15 +0200

    Finished upgrade to angular 14

--- libs/mintplayer-ng-bootstrap/src/lib/pipes/in-list/in-list.pipe.spec.ts
+++ libs/mintplayer-ng-bootstrap/src/lib/pipes/in-list/in-list.pipe.spec.ts
@@ -1,8 +1,13 @@
 import { BsInListPipe } from './in-list.pipe';
 
 describe('BsInListPipe', () => {
+  jest.setTimeout(30000);
   it('create an instance', () => {
     const pipe = new BsInListPipe();
     expect(pipe).toBeTruthy();
+  });
+
+  afterAll(async () => {
+    await new Promise(resolve => setTimeout(resolve, 1000)); // avoid jest open handle error
   });
 });
