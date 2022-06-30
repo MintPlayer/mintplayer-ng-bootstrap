9572bcf94b07024a9351ec1894e3c3947a2c33bc
Pieterjan De Clippel <pieterjandeclippel@msn.com>
2022-06-30 08:53:20 +0200

    Temp commit

--- apps/ng-bootstrap-demo/src/app/pages/additional-samples/swiper/swiper.component.ts
+++ apps/ng-bootstrap-demo/src/app/pages/additional-samples/swiper/swiper.component.ts
@@ -1,15 +1,56 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { BehaviorSubject, map, Observable, Subject } from 'rxjs';
 
 @Component({
   selector: 'demo-swiper',
   templateUrl: './swiper.component.html',
   styleUrls: ['./swiper.component.scss']
 })
-export class SwiperComponent implements OnInit {
+export class SwiperComponent implements OnInit, OnDestroy {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    this.imageData$ = this.images$
+      .pipe(map((images) => images.map(image => <ImageData>{
+        url: image,
+        marginLeft: undefined
+      })));
   }
 
+  imageIndex$ = new BehaviorSubject<number>(0);
+  isSwiping$ = new BehaviorSubject<boolean>(false);
+  destroyed$ = new Subject();
+
+  images$ = new BehaviorSubject<string[]>([
+    '/assets/resized/deer.png',
+    '/assets/resized/duck.png',
+    '/assets/resized/leopard.png',
+    '/assets/resized/lion.png',
+    '/assets/resized/peacock.png',
+    '/assets/resized/tiger.png',
+  ]);
+
+  imageData$: Observable<ImageData[]>;
+
+  ngOnInit() {
+  }
+
+  ngOnDestroy(): void {
+    this.destroyed$.next(true);
+  }
+  
+  onSwipeStart = () => {
+    this.isSwiping$.next(true);
+  }
+
+  onSwipeEnd = () =>  {
+    this.isSwiping$.next(false);
+    this.imageIndex$.next(this.imageIndex$.value + 1);
+    return false;
+  }
+
+}
+
+export interface ImageData {
+  url: string;
+  marginLeft: number | undefined;
 }
--- libs/mintplayer-ng-swiper/src/lib/directives/swipe/swipe.directive.ts
+++ libs/mintplayer-ng-swiper/src/lib/directives/swipe/swipe.directive.ts
@@ -1,5 +1,7 @@
 import { animate, AnimationBuilder, style } from '@angular/animations';
-import { Directive, ElementRef, HostBinding, HostListener, Input } from '@angular/core';
+import { Directive, ElementRef, EventEmitter, HostBinding, HostListener, Input, Output } from '@angular/core';
+import { Point } from '../../interfaces/point';
+import { Direction } from '../../types/direction';
 
 @Directive({
   selector: '[swipe]'
@@ -9,31 +11,81 @@ export class SwipeDirective {
   constructor(private element: ElementRef, private animationBuilder: AnimationBuilder) {
   }
 
-  start: number | null = null;
-  @HostBinding('style.margin-left.px') offsetLeft = 0;
-  @HostBinding('style.margin-right.px') offsetRight = 0;
+  start: Point | null = null;
+  @HostBinding('style.margin-left.px') offsetLeft: number | null = null;
+  @HostBinding('style.margin-right.px') offsetRight: number | null = null;
+  @HostBinding('style.margin-top.px') offsetTop: number | null = null;
+  @HostBinding('style.margin-bottom.px') offsetBottom: number | null = null;
 
-  private get offset() {
+  @Input() public correctRight = true;
+
+  private get offsetX() {
     return this.offsetRight;
   }
-  private set offset(value: number) {
-    this.offsetLeft = -value;
-    this.offsetRight = value;
+  private set offsetX(value: number | null) {
+    if (value) {
+      this.offsetLeft = -value;
+      this.offsetRight = this.correctRight ? value : 0;
+    } else {
+      this.offsetLeft = null;
+      this.offsetRight = null;
+    }
+  }
+
+  private get offsetY() {
+    return this.offsetBottom;
+  }
+  private set offsetY(value: number | null) {
+    if (value) {
+      this.offsetTop = -value;
+      this.offsetBottom = value;
+    } else {
+      this.offsetTop = null;
+      this.offsetBottom = null;
+    }
   }
 
   @Input() public easeOut = true;
+  @Input() public allowedDirections: Direction[] = [];
+
+  @Input() public swipeStart: (() => void) | null = null;
+  @Input() public swipeEnd: (() => boolean) | null = null;
 
   @HostListener('touchstart', ['$event']) onTouchStart(ev: TouchEvent) {
     if (ev.touches.length === 1) {
       ev.preventDefault();
-      this.start = ev.touches[0].clientX;
+      this.start = {
+        x: ev.touches[0].clientX,
+        y: ev.touches[0].clientY
+      };
+
+      if (this.swipeStart) {
+        this.swipeStart();
+      }
     }
   }
   
   @HostListener('touchmove', ['$event']) onTouchMove(ev: TouchEvent) {
     if (this.start) {
       ev.preventDefault();
-      this.offset = (this.start - ev.touches[0].clientX);
+
+      let offsetX = this.start.x - ev.touches[0].clientX;
+      if (!this.allowedDirections.includes('left') && (offsetX < 0)) {
+        offsetX = 0;
+      }
+      if (!this.allowedDirections.includes('right') && (offsetX > 0)) {
+        offsetX = 0;
+      }
+      this.offsetX = offsetX;
+
+      let offsetY = this.start.y - ev.touches[0].clientY;
+      if (!this.allowedDirections.includes('up') && (offsetY < 0)) {
+        offsetY = 0;
+      }
+      if (!this.allowedDirections.includes('down') && (offsetY > 0)) {
+        offsetY = 0;
+      }
+      this.offsetY = offsetY;
     }
   }
 
@@ -42,19 +94,36 @@ export class SwipeDirective {
       ev.preventDefault();
       this.start = null;
 
-      if (this.easeOut) {
-        const animation = this.animationBuilder.build([
-          style({ 'margin-left': this.offsetLeft + 'px', 'margin-right': this.offsetRight + 'px' }),
-          animate('100ms', style({ 'margin-left': 0, 'margin-right': 0 }))
-        ]);
-        const player = animation.create(this.element.nativeElement);
-        player.onDone(() => {
-          this.offset = 0;
-          player.destroy();
-        });
-        player.play();
+      if (this.swipeEnd && this.swipeEnd()) {
+        console.log('swipeEnd handled');
+        this.offsetX = this.offsetY = null;
       } else {
-        this.offset = 0;
+        if (this.easeOut) {
+          const animation = this.animationBuilder.build([
+            style({
+              'margin-left': this.offsetLeft + 'px',
+              'margin-right': this.offsetRight + 'px',
+              'margin-top': this.offsetTop + 'px',
+              'margin-bottom': this.offsetBottom + 'px',
+            }),
+            animate('100ms', style({
+              'margin-left': 0,
+              'margin-right': 0,
+              'margin-top': 0,
+              'margin-bottom': 0,
+            }))
+          ]);
+          const player = animation.create(this.element.nativeElement);
+          player.onDone(() => {
+            this.offsetX = 0;
+            this.offsetY = 0;
+            player.destroy();
+          });
+          player.play();
+        } else {
+          this.offsetX = 0;
+          this.offsetY = 0;
+        }
       }
     }
   }
