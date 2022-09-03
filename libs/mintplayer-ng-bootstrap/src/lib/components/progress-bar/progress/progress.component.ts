3f5fae268fd60312c4b1f63e3ccd2ce19ef57716
Pieterjan De Clippel <pieterjandeclippel@msn.com>
2022-09-03 22:04:43 +0200

    Move styles to components

--- libs/mintplayer-ng-bootstrap/src/lib/components/progress-bar/progress/progress.component.ts
+++ libs/mintplayer-ng-bootstrap/src/lib/components/progress-bar/progress/progress.component.ts
@@ -6,7 +6,12 @@ import { Component, HostBinding, Input } from '@angular/core';
   styleUrls: ['./progress.component.scss']
 })
 export class BsProgressComponent {
-  @HostBinding('class.progress') private progressClass = true;
+  // @HostBinding('class.progress') private progressClass = true;
   @Input() @HostBinding('style.height.px') public height: number | null = null;
   @Input() public isIndeterminate = false;
+
+  @HostBinding('class.d-block')
+  @HostBinding('class.overflow-hidden')
+  private progressClass = true;
+
 }
