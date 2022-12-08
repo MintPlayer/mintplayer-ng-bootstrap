dfb0bd2a9f2babcfb87eb37736bde254c217b71c
Pieterjan De Clippel <pieterjandeclippel@msn.com>
2022-12-09 00:12:29 +0100

    Fix datatable

--- libs/mintplayer-ng-bootstrap/datatable/src/datatable.module.ts
+++ libs/mintplayer-ng-bootstrap/datatable/src/datatable.module.ts
@@ -1,25 +1,28 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-// import { BsGridModule } from '@mintplayer/ng-bootstrap/grid';
+import { BsGridModule } from '@mintplayer/ng-bootstrap/grid';
 import { BsTableModule } from '@mintplayer/ng-bootstrap/table';
-// import { BsPaginationModule } from '@mintplayer/ng-bootstrap/pagination';
+import { BsPaginationModule } from '@mintplayer/ng-bootstrap/pagination';
 import { BsDatatableComponent } from './datatable/datatable.component';
 import { BsDatatableColumnDirective } from './datatable-column';
+import { BsRowTemplateDirective } from './row-template/row-template.directive';
 
 @NgModule({
   declarations: [
     BsDatatableComponent,
-    BsDatatableColumnDirective
+    BsDatatableColumnDirective,
+    BsRowTemplateDirective
   ],
   imports: [
     CommonModule,
-    // BsGridModule,
+    BsGridModule,
     BsTableModule,
-    // BsPaginationModule
+    BsPaginationModule
   ],
   exports: [
     BsDatatableComponent,
-    BsDatatableColumnDirective
+    BsDatatableColumnDirective,
+    BsRowTemplateDirective
   ]
 })
 export class BsDatatableModule { }
