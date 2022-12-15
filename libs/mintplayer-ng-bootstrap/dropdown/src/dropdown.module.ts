40482b1fa75049c4c20a7a423a6f6c6d11bb9f14
Pieterjan De Clippel <pieterjandeclippel@msn.com>
2022-12-15 13:37:51 +0100

    Replace level-up scss-imports with non-prefixed path

--- libs/mintplayer-ng-bootstrap/dropdown/src/dropdown.module.ts
+++ libs/mintplayer-ng-bootstrap/dropdown/src/dropdown.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { OverlayModule } from '@angular/cdk/overlay';
 import { ClickOutsideModule } from '@mintplayer/ng-click-outside';
+import { BsHasOverlayModule } from '@mintplayer/ng-bootstrap/has-overlay';
 import { BsDropdownDirective } from './dropdown/dropdown.directive';
 import { BsDropdownMenuDirective } from './dropdown-menu/dropdown-menu.directive';
 import { BsDropdownToggleDirective } from './dropdown-toggle/dropdown-toggle.directive';
@@ -15,12 +16,13 @@ import { BsDropdownToggleDirective } from './dropdown-toggle/dropdown-toggle.dir
     BsDropdownMenuDirective,
     // BsDropdownComponent,
   ],
-  imports: [CommonModule, OverlayModule, ClickOutsideModule],
+  imports: [CommonModule, OverlayModule, BsHasOverlayModule, ClickOutsideModule],
   exports: [
     BsDropdownDirective,
     BsDropdownToggleDirective,
     BsDropdownMenuDirective,
     // BsDropdownComponent,
+    BsHasOverlayModule,
   ],
 })
 export class BsDropdownModule {}
