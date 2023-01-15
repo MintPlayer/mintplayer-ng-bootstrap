81124317264704ba3008d3ff95b60ddb0c084894
Pieterjan De Clippel <pieterjandeclippel@msn.com>
2023-01-15 15:33:51 +0100

    Moved rating

--- libs/mintplayer-ng-bootstrap/rating/src/rating.module.ts
+++ libs/mintplayer-ng-bootstrap/rating/src/rating.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { BsRatingComponent } from './rating.component';
+import { BsRatingComponent } from './component/rating.component';
 
 
 
