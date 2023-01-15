32ed5767d3c344d89c3838f90f473387fd6a0a37
Pieterjan De Clippel <pieterjandeclippel@msn.com>
2023-01-15 15:12:11 +0100

    Typed bsColumn directive

--- libs/mintplayer-ng-bootstrap/grid/src/index.ts
+++ libs/mintplayer-ng-bootstrap/grid/src/index.ts
@@ -1,3 +1,4 @@
 export * from './component/grid.component';
 export * from './directives';
 export * from './grid.module';
+export * from './interfaces';
---
+++ libs/mintplayer-ng-bootstrap/grid/src/interfaces/column-definition.ts
@@ -0,0 +1,9 @@
+export interface BsColumnDefinition {
+    _?: number;
+    xs?: number;
+    sm?: number;
+    md?: number;
+    lg?: number;
+    xl?: number;
+    xxl?: number;
+}
