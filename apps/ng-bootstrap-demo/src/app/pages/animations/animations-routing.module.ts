7e2ad0b20a6a5fa3ef222f8e663772b353ca44e0
Pieterjan <pieterjandeclippel@msn.com>
2023-01-29 12:06:21 +0100

    Conditionally add routerlink (#51)

--- apps/ng-bootstrap-demo/src/app/pages/animations/animations-routing.module.ts
+++ apps/ng-bootstrap-demo/src/app/pages/animations/animations-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+  { path: '', redirectTo: '', pathMatch: 'full', canActivate: [() => false] },
   { path: 'slide-up-down', loadChildren: () => import('./slide-up-down/slide-up-down.module').then(m => m.SlideUpDownModule) },
   { path: 'fade-in-out', loadChildren: () => import('./fade-in-out/fade-in-out.module').then(m => m.FadeInOutModule) },
   { path: 'color-transition', loadChildren: () => import('./color-transition/color-transition.module').then(m => m.ColorTransitionModule) }
