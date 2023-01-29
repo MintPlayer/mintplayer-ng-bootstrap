2fe2103452e29068867ae724b94d09b5ba01a860
Pieterjan De Clippel <pieterjandeclippel@msn.com>
2023-01-29 11:59:27 +0100

    Conditionally add routerlink

--- apps/ng-bootstrap-demo/src/app/app.component.ts
+++ apps/ng-bootstrap-demo/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component, Inject } from '@angular/core';
+import { isPlatformServer } from '@angular/common';
+import { Component, Inject, PLATFORM_ID } from '@angular/core';
 import { SlideUpDownAnimation } from '@mintplayer/ng-animations';
 import { Color } from '@mintplayer/ng-bootstrap';
 
@@ -9,10 +10,12 @@ import { Color } from '@mintplayer/ng-bootstrap';
   animations: [SlideUpDownAnimation]
 })
 export class AppComponent {
-  constructor(@Inject('BOOTSTRAP_VERSION') bootstrapVersion: string) {
+  constructor(@Inject('BOOTSTRAP_VERSION') bootstrapVersion: string, @Inject(PLATFORM_ID) platformId: any) {
     this.versionInfo = bootstrapVersion;
+    this.isServerSide = isPlatformServer(platformId);
   }
 
   versionInfo = '';
   colors = Color;
+  isServerSide: boolean;
 }
