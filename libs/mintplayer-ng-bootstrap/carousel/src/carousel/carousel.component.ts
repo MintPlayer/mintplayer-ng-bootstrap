ec681a12186b6ef769880ae5f4ea861750c51d1c
Pieterjan De Clippel <pieterjandeclippel@msn.com>
2023-02-21 09:43:13 +0100

    Carousel - Add image observables

--- libs/mintplayer-ng-bootstrap/carousel/src/carousel/carousel.component.ts
+++ libs/mintplayer-ng-bootstrap/carousel/src/carousel/carousel.component.ts
@@ -1,8 +1,9 @@
 import { isPlatformServer } from '@angular/common';
-import { ChangeDetectorRef, Component, ContentChildren, forwardRef, HostBinding, HostListener, Inject, Input, PLATFORM_ID, QueryList, ViewChild } from '@angular/core';
+import { ChangeDetectorRef, Component, ContentChildren, forwardRef, HostBinding, HostListener, Inject, Input, PLATFORM_ID, QueryList, TemplateRef, ViewChild } from '@angular/core';
 import { FadeInOutAnimation } from '@mintplayer/ng-animations';
 import { Color } from '@mintplayer/ng-bootstrap';
 import { BsSwipeContainerDirective } from '@mintplayer/ng-swiper';
+import { BehaviorSubject, map, Observable } from 'rxjs';
 import { BsCarouselImageDirective } from '../carousel-image/carousel-image.directive';
 
 @Component({
@@ -15,16 +16,42 @@ export class BsCarouselComponent {
 
   constructor(@Inject(PLATFORM_ID) platformId: any, private cdRef: ChangeDetectorRef) {
     this.isServerSide = isPlatformServer(platformId);
+    this.imageCount$ = this.images$.pipe(map((images) => images?.length ?? 0));
+    this.firstImageTemplate$ = this.images$.pipe(map((images) => {
+      if (!images) return null;
+      if (images.length === 0) return null;
+
+      const img = images.get(0);
+      if (!img) return null;
+
+      return img.itemTemplate;
+    }));
+    this.lastImageTemplate$ = this.images$.pipe(map((images) => {
+      if (!images) return null;
+      if (images.length === 0) return null;
+      
+      const img = images.get(images.length - 1);
+      if (!img) return null;
+
+      return img.itemTemplate;
+    }));
   }
   
   colors = Color;
   isServerSide: boolean;
   currentImageIndex = 0;
+  images$ = new BehaviorSubject<QueryList<BsCarouselImageDirective> | null>(null);
+  imageCount$: Observable<number>;
+  firstImageTemplate$: Observable<TemplateRef<any> | null>;
+  lastImageTemplate$: Observable<TemplateRef<any> | null>;
+
   @Input() indicators = false;
   @Input() keyboardEvents = true;
 
   @ViewChild('container') swipeContainer!: BsSwipeContainerDirective;
-  @ContentChildren(forwardRef(() => BsCarouselImageDirective)) images!: QueryList<BsCarouselImageDirective>;
+  @ContentChildren(forwardRef(() => BsCarouselImageDirective)) set images(value: QueryList<BsCarouselImageDirective>) {
+    this.images$.next(value);
+  }
 
   //#region Animation
   @HostBinding('@.disabled') public animationsDisabled = false;
