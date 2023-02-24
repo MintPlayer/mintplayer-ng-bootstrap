6350275a32533f448c6773b72dc071d6182960d3
Pieterjan De Clippel <pieterjandeclippel@msn.com>
2023-02-24 12:48:45 +0100

    temp commit

--- libs/mintplayer-ng-bootstrap/dock/src/dock-panel/dock-panel.component.ts
+++ libs/mintplayer-ng-bootstrap/dock/src/dock-panel/dock-panel.component.ts
@@ -1,4 +1,5 @@
-import { Component, HostBinding } from '@angular/core';
+import { CdkDragMove } from '@angular/cdk/drag-drop';
+import { Component, ElementRef, HostBinding, ViewChild, NgZone } from '@angular/core';
 
 @Component({
   selector: 'bs-dock-panel',
@@ -6,5 +7,23 @@ import { Component, HostBinding } from '@angular/core';
   styleUrls: ['./dock-panel.component.scss'],
 })
 export class BsDockPanelComponent {
+  constructor(private zone: NgZone) {
+  }
+
+  @ViewChild('dockPanel') dockPanel!: ElementRef<HTMLDivElement>;
+  
   @HostBinding('class.d-block') dBlockClass = true;
+  onDragMove(element: HTMLDivElement, ev: CdkDragMove<any>) {
+    console.log('dragmove', { element, ev, panel: this.dockPanel });
+    this.zone.runOutsideAngular(() => {
+      const dragRect = element.getBoundingClientRect();
+      const targetRect = this.dockPanel.nativeElement.getBoundingClientRect();
+  
+      const width = dragRect.left - targetRect.left + dragRect.width;
+      const height = dragRect.top - targetRect.top + dragRect.height;
+  
+      this.dockPanel.nativeElement.style.width = (width - 5) + 'px';
+      this.dockPanel.nativeElement.style.height = (height - 5) + 'px';
+    });
+  }
 }
