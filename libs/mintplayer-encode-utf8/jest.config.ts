ac9188ebe27ec0a4b830e4394915911eea92f425
Pieterjan De Clippel <pieterjandeclippel@msn.com>
2023-03-03 09:18:26 +0100

    Updated

--- libs/mintplayer-encode-utf8/jest.config.ts
+++ libs/mintplayer-encode-utf8/jest.config.ts
@@ -2,13 +2,14 @@
 export default {
   displayName: 'mintplayer-encode-utf8',
   preset: '../../jest.preset.js',
-  globals: {
-    'ts-jest': {
-      tsconfig: '<rootDir>/tsconfig.spec.json',
-    },
-  },
+  globals: {},
   transform: {
-    '^.+\\.[tj]s$': 'ts-jest',
+    '^.+\\.[tj]s$': [
+      'ts-jest',
+      {
+        tsconfig: '<rootDir>/tsconfig.spec.json',
+      },
+    ],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
   coverageDirectory: '../../coverage/libs/mintplayer-encode-utf8',
