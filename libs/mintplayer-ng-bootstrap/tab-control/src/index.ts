50d1c117342cbb7b2f5fe788cc3b5dd101d9a54c
Pieterjan De Clippel <pieterjandeclippel@msn.com>
2023-03-26 23:21:29 +0200

    expose tab-page-header

--- libs/mintplayer-ng-bootstrap/tab-control/src/index.ts
+++ libs/mintplayer-ng-bootstrap/tab-control/src/index.ts
@@ -1,4 +1,5 @@
 export * from './tab-control.module';
 export * from './tab-control/tab-control.component';
 export * from './tab-page/tab-page.component';
+export * from './tab-page-header/tab-page-header.directive';
 export * from './tabs-position';
