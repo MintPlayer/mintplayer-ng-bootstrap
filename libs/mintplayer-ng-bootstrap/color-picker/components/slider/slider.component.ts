044b57d08f3c1791468981539b42655d37a62861
Pieterjan De Clippel <pieterjandeclippel@msn.com>
2023-04-02 18:43:43 +0200

    hs no longer necessary

--- libs/mintplayer-ng-bootstrap/color-picker/components/slider/slider.component.ts
+++ libs/mintplayer-ng-bootstrap/color-picker/components/slider/slider.component.ts
@@ -24,15 +24,6 @@ export class BsSliderComponent implements OnDestroy {
   @ViewChild('track') track!: ElementRef<HTMLDivElement>;
   @ViewChild('thumb') thumb!: ElementRef<HTMLDivElement>;
 
-  //#region HS
-  hs$ = new BehaviorSubject<HS>({ hue: 0, saturation: 0 });
-  public get hs() {
-    return this.hs$.value;
-  }
-  @Input() public set hs(value: HS) {
-    this.hs$.next(value);
-  }
-  //#endregion
   //#region Value
   value$ = new BehaviorSubject<number>(0.5);
   @Output() valueChange = new EventEmitter<number>();
