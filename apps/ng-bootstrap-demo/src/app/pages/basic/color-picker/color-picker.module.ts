1cf011ba80b88318b962340504db2eae251fd385
Pieterjan De Clippel <pieterjandeclippel@msn.com>
2023-04-03 09:15:44 +0200

    Only set ColorPicker color through FormsModule

--- apps/ng-bootstrap-demo/src/app/pages/basic/color-picker/color-picker.module.ts
+++ apps/ng-bootstrap-demo/src/app/pages/basic/color-picker/color-picker.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { BsGridModule } from '@mintplayer/ng-bootstrap/grid';
+import { BsListGroupModule } from '@mintplayer/ng-bootstrap/list-group';
 import { BsButtonTypeModule } from '@mintplayer/ng-bootstrap/button-type';
 import { BsButtonGroupModule } from '@mintplayer/ng-bootstrap/button-group';
 import { BsColorPickerModule } from '@mintplayer/ng-bootstrap/color-picker';
@@ -17,10 +19,12 @@ import { BsToggleButtonModule } from '@mintplayer/ng-bootstrap/toggle-button';
   imports: [
     CommonModule,
     FormsModule,
+    BsGridModule,
     BsButtonTypeModule,
     BsButtonGroupModule,
     BsColorPickerModule,
     BsToggleButtonModule,
+    BsListGroupModule,
     ColorPickerRoutingModule
   ]
 })
--- libs/mintplayer-ng-bootstrap/color-picker/components/color-picker/color-picker.component.ts
+++ libs/mintplayer-ng-bootstrap/color-picker/components/color-picker/color-picker.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, Input, ViewChild } from "@angular/core";
+import { Component, Input, ViewChild } from "@angular/core";
 import { BehaviorSubject } from "rxjs";
 import { HS } from "../../interfaces/hs";
 import { BsColorWheelComponent } from "../color-wheel/color-wheel.component";
@@ -20,31 +20,15 @@ export class BsColorPickerComponent {
     console.log('allowAlpha', value);
     this.allowAlpha$.next(value);
   }
-
   
   width$ = new BehaviorSubject<number>(150);
   height$ = new BehaviorSubject<number>(150);
   disabled$ = new BehaviorSubject<boolean>(false);
   allowAlpha$ = new BehaviorSubject<boolean>(true);
 
-  //#region HS
   hs$ = new BehaviorSubject<HS>({ hue: 0, saturation: 0 });
-  get hs() {
-    return this.hs$.value;
-  }
-  @Input() set hs(value: HS) {
-    this.hs$.next(value);
-  }
-  //#endregion
-  //#region Luminosity
   luminosity$ = new BehaviorSubject<number>(0);
-  get luminosity() {
-    return this.luminosity$.value;
-  }
-  @Input() set luminosity(value: number) {
-    this.luminosity$.next(value);
-  }
-  //#endregion
+
   //#region Alpha
   alpha$ = new BehaviorSubject<number>(1);
   get alpha() {
