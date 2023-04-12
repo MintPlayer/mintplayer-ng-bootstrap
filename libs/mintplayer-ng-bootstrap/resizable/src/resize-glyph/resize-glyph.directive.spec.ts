e2889ffb0aa94c47c804a80509e991340d2c31d8
Pieterjan De Clippel <pieterjandeclippel@msn.com>
2023-04-12 08:57:24 +0200

    Removed impossible unit test

--- libs/mintplayer-ng-bootstrap/resizable/src/resize-glyph/resize-glyph.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { BsResizeGlyphDirective } from './resize-glyph.directive';
-
-describe('BsResizeGlyphDirective', () => {
-  it('should create an instance', () => {
-    const directive = new BsResizeGlyphDirective();
-    expect(directive).toBeTruthy();
-  });
-});
