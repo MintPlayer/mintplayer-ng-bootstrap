c63eafd8e8c07830aa754966a80d1c3a6370a01b
Pieterjan De Clippel <pieterjandeclippel@msn.com>
2023-04-20 21:36:14 +0200

    Fixed unit test

--- libs/mintplayer-parentify/src/lib/parentify.service.spec.ts
+++ libs/mintplayer-parentify/src/lib/parentify.service.spec.ts
@@ -2,6 +2,6 @@ import { deepClone } from './parentify.service';
 
 describe('deepClone', () => {
   it('should work', () => {
-    expect(deepClone({}, false)).toEqual({});
+    expect(deepClone({}, false, [Object], false)).toEqual({ $original: {} });
   });
 });
