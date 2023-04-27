d1c2b3860770a287b136db9c20b910a0461423f6
Pieterjan De Clippel <pieterjandeclippel@msn.com>
2023-04-27 10:21:04 +0200

    Removed ObserverModule

--- libs/mintplayer-ng-swiper/src/lib/swiper.module.ts
+++ libs/mintplayer-ng-swiper/src/lib/swiper.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ObserversModule } from '@angular/cdk/observers';
 import { BsSwipeDirective } from './directives/swipe/swipe.directive';
 import { BsSwipeContainerDirective } from './directives/swipe-container/swipe-container.directive';
 
 @NgModule({
-  imports: [CommonModule, ObserversModule],
+  imports: [CommonModule],
   declarations: [BsSwipeDirective, BsSwipeContainerDirective],
   exports: [BsSwipeDirective, BsSwipeContainerDirective],
 })
