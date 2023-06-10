841beb2257ad7b3f566dc4044c22ae0ed6d0cbd2
Pieterjan <pieterjandeclippel@msn.com>
2023-06-10 19:40:31 +0200

    Improved carousel (#129)
    * Improved carousel
    
    * Fixed test
    
    * Remove empty binding
    
    * Only deploy when new version published

--- apps/ng-bootstrap-demo/src/app/pages/advanced/signature-pad/signature-pad.component.spec.ts
+++ apps/ng-bootstrap-demo/src/app/pages/advanced/signature-pad/signature-pad.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SignaturePadComponent } from './signature-pad.component';
+import { MockModule } from 'ng-mocks';
+import { BsSignaturePadModule } from '@mintplayer/ng-bootstrap/signature-pad';
 
 describe('SignaturePadComponent', () => {
   let component: SignaturePadComponent;
@@ -8,7 +10,12 @@ describe('SignaturePadComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [SignaturePadComponent]
+      imports: [
+        MockModule(BsSignaturePadModule)
+      ],
+      declarations: [
+        SignaturePadComponent
+      ]
     });
     fixture = TestBed.createComponent(SignaturePadComponent);
     component = fixture.componentInstance;
