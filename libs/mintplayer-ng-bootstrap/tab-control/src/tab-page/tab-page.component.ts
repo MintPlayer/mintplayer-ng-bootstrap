1fc6360f98bd9890fc9d3fa18c24d04b8b330c4a
Pieterjan De Clippel <pieterjandeclippel@msn.com>
2023-07-07 14:24:39 +0200

    Temp commit

--- libs/mintplayer-ng-bootstrap/tab-control/src/tab-page/tab-page.component.ts
+++ libs/mintplayer-ng-bootstrap/tab-control/src/tab-page/tab-page.component.ts
@@ -1,5 +1,6 @@
-import { Component, ContentChild, TemplateRef, Input, ElementRef } from '@angular/core';
-import { BehaviorSubject, combineLatest, map, Observable } from 'rxjs';
+import { Component, ContentChild, TemplateRef, Input, ElementRef, Inject, PLATFORM_ID } from '@angular/core';
+import { isPlatformServer } from '@angular/common';
+import { BehaviorSubject, combineLatest, map, Observable, of } from 'rxjs';
 import { BsTabControlComponent } from '../tab-control/tab-control.component';
 import { BsTabPageHeaderDirective } from '../tab-page-header/tab-page-header.directive';
 
@@ -10,18 +11,30 @@ import { BsTabPageHeaderDirective } from '../tab-page-header/tab-page-header.dir
 })
 export class BsTabPageComponent {
 
-  constructor(tabControl: BsTabControlComponent, element: ElementRef<any>) {
+  constructor(@Inject(PLATFORM_ID) platformId: Object, tabControl: BsTabControlComponent, element: ElementRef<any>) {
     this.element = element;
     this.tabControl = tabControl;
+    this.isServerSide = isPlatformServer(platformId);
     this.tabId$ = new BehaviorSubject<number>(++this.tabControl.tabCounter);
     this.tabName$ = combineLatest([this.tabControl.tabControlName$, this.tabId$])
       .pipe(map(([tabControlName, tabId]) =>  `${tabControlName}-${tabId}`));
+    this.isSelected$ = tabControl.activeTab$.pipe(map(activeTab => activeTab === this));
+    this.dBlockClass$ = this.isSelected$.pipe(map((isSelected) => {
+      if (this.isServerSide) {
+        return false;
+      } else {
+        return isSelected;
+      }
+    }));
   }
 
   element: ElementRef<any>;
   tabControl: BsTabControlComponent;
   tabId$: BehaviorSubject<number>;
   tabName$: Observable<string>;
+  isSelected$: Observable<boolean>;
+  dBlockClass$: Observable<boolean>;
+  isServerSide: boolean;
 
   @Input() disabled = false;
   @ContentChild(BsTabPageHeaderDirective) headerTemplate!: BsTabPageHeaderDirective;
