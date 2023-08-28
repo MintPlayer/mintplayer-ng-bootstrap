206af9e5426bf234cfeb2ec007c6a821ff0a81f5
Pieterjan De Clippel <pieterjandeclippel@msn.com>
2023-08-28 08:56:08 +0200

    Fixed export

--- libs/mintplayer-ng-bootstrap/color-picker/components/index.ts
+++ libs/mintplayer-ng-bootstrap/color-picker/components/index.ts
@@ -1,3 +1,4 @@
+export * from './alpha-strip/alpha-strip.component';
 export * from './color-picker/color-picker.component';
 export * from './color-wheel/color-wheel.component';
 export * from './luminosity-strip/luminosity-strip.component';
