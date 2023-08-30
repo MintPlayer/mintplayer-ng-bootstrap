bb7b4de3274d4dee075b5ba8fcdd52e9ba22e63a
Pieterjan De Clippel <pieterjandeclippel@msn.com>
2023-08-30 16:22:10 +0200

    Fixed unit tests

--- libs/mintplayer-ng-bootstrap/sticky-footer/src/sticky-footer/sticky-footer.component.spec.ts
+++ libs/mintplayer-ng-bootstrap/sticky-footer/src/sticky-footer/sticky-footer.component.spec.ts
@@ -1,16 +1,42 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { StickyFooterComponent } from './sticky-footer.component';
+global.ResizeObserver = require('resize-observer-polyfill');
 
-describe('StickyFooterComponent', () => {
-  let component: StickyFooterComponent;
-  let fixture: ComponentFixture<StickyFooterComponent>;
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MockDirective } from 'ng-mocks';
+import { Component } from '@angular/core';
+import { BsStickyFooterComponent } from './sticky-footer.component';
+import { BsStickyFooterParentDirective } from '../sticky-footer-parent/sticky-footer-parent.directive';
+
+@Component({
+  selector: 'sticky-footer-test',
+  template: `
+    <div bsStickyFooterParent>
+      <bs-sticky-footer>
+        Content
+      </bs-sticky-footer>
+    </div>`
+})
+class BsStickyFooterTestComponent {
+}
+
+describe('BsStickyFooterComponent', () => {
+  let component: BsStickyFooterTestComponent;
+  let fixture: ComponentFixture<BsStickyFooterTestComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [StickyFooterComponent],
+      declarations: [
+        // Unit test
+        BsStickyFooterComponent,
+      
+        // Mock dependencies
+        MockDirective(BsStickyFooterParentDirective),
+
+        // Testbench
+        BsStickyFooterTestComponent
+      ],
     }).compileComponents();
 
-    fixture = TestBed.createComponent(StickyFooterComponent);
+    fixture = TestBed.createComponent(BsStickyFooterTestComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
