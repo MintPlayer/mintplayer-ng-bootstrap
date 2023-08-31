475e6661976746c2107d0f3498c5df803f66ddae
Pieterjan <pieterjandeclippel@msn.com>
2023-08-31 09:46:17 +0200

    ResizeObserver not available in NodeJS (#138)

--- libs/mintplayer-ng-bootstrap/sticky-footer/src/sticky-footer/sticky-footer.component.ts
+++ libs/mintplayer-ng-bootstrap/sticky-footer/src/sticky-footer/sticky-footer.component.ts
@@ -1,5 +1,6 @@
-import { Component, AfterViewInit, ElementRef, ViewChild, OnDestroy } from '@angular/core';
+import { Component, AfterViewInit, ElementRef, ViewChild, OnDestroy, Inject, PLATFORM_ID } from '@angular/core';
 import { BsStickyFooterParentDirective } from '../sticky-footer-parent/sticky-footer-parent.directive';
+import { isPlatformServer } from '@angular/common';
 
 @Component({
   selector: 'bs-sticky-footer',
@@ -7,20 +8,22 @@ import { BsStickyFooterParentDirective } from '../sticky-footer-parent/sticky-fo
   styleUrls: ['./sticky-footer.component.scss'],
 })
 export class BsStickyFooterComponent implements AfterViewInit, OnDestroy {
-  constructor(private parent: BsStickyFooterParentDirective, private element: ElementRef) {
-    this.resizeObserver = new ResizeObserver((entries) => {
-      this.parent.marginBottom = entries[0].contentRect.height;
-    });
+  constructor(private parent: BsStickyFooterParentDirective, private element: ElementRef, @Inject(PLATFORM_ID) private platformId: any) {
+    if (!isPlatformServer(this.platformId)) {
+      this.resizeObserver = new ResizeObserver((entries) => {
+        this.parent.marginBottom = entries[0].contentRect.height;
+      });
+    }
   }
 
-  resizeObserver: ResizeObserver;
+  resizeObserver?: ResizeObserver;
   @ViewChild('foot') footer!: ElementRef<HTMLElement>;
 
   ngAfterViewInit() {
-    this.resizeObserver.observe(this.footer.nativeElement);
+    this.resizeObserver && this.resizeObserver.observe(this.footer.nativeElement);
   }
 
   ngOnDestroy() {
-    this.resizeObserver.unobserve(this.footer.nativeElement);
+    this.resizeObserver && this.resizeObserver.unobserve(this.footer.nativeElement);
   }
 }
