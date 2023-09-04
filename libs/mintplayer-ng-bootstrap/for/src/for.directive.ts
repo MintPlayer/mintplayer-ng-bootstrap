62c2f22b5ba301f5b6cc2274c4d2bbd37f243a02
Pieterjan De Clippel <pieterjandeclippel@msn.com>
2023-09-04 21:54:55 +0200

    Make for-directive noscript friendly

--- libs/mintplayer-ng-bootstrap/for/src/for.directive.ts
+++ libs/mintplayer-ng-bootstrap/for/src/for.directive.ts
@@ -1,23 +1,42 @@
 import { Directive, HostBinding, HostListener, Input } from '@angular/core';
 
 @Directive({
-  selector: '[bsFor]'
+  selector: 'label[bsFor]'
 })
 export class BsForDirective {
 
   @HostBinding('class.cursor-default') defaultCursor = true;
 
-  @Input() bsFor: any;
-  @HostListener('click') onMouseClick() {
-    if (!('tagName' in this.bsFor)) {
-      this.bsFor.focus();
-    } else if (this.bsFor.tagName.toLowerCase() !== 'input') {
-      this.bsFor.focus();
-    } else if (this.bsFor.type.toLowerCase() === 'file') {
-      this.bsFor.click();
-    } else {
-      this.bsFor.focus();
+  static counter = 1;
+  private target: any;
+  @Input() public set bsFor(value: any) {
+    this.target = value;
+    if (this.target instanceof HTMLElement) {
+      if (!this.target.id) {
+        const count = BsForDirective.counter++;
+        this.target.id = `for-target-${count}`;
+      }
+      this.forValue = this.target.id;
     }
+    // console.log('target', this.target);
   }
+
+  @HostBinding('attr.for') forValue?: string;
+  
+  // @HostListener('click') onMouseClick() {
+  //   if (!('tagName' in this.target)) {
+  //     // Not a form-control, let's just assume the class has some "focus" method
+  //     this.target.focus();
+  //   } else if (this.target.tagName.toLowerCase() !== 'input') {
+  //     // HTML element, but not an input, probably a textarea
+  //     this.target.focus();
+  //   } else if (this.target.type.toLowerCase() === 'file') {
+  //     // HtmlInputElement for a file
+  //     this.target.click();
+  //   } else {
+  //     // HtmlInputElement
+  //     this.target.focus();
+  //   }
+  // }
 
 }
