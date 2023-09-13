e9c2d46f170410f0c8ff6a1db9cd2908d1d27fea
Pieterjan <pieterjandeclippel@msn.com>
2023-09-13 23:51:27 +0200

    Add intellisense to template variables (#147)

--- libs/mintplayer-ng-bootstrap/navbar/src/expand-button/expand-button.directive.ts
+++ libs/mintplayer-ng-bootstrap/navbar/src/expand-button/expand-button.directive.ts
@@ -1,4 +1,5 @@
 import { Directive, TemplateRef } from '@angular/core';
+import { BsViewState } from '@mintplayer/ng-bootstrap';
 import { BsNavbarComponent } from '../navbar/navbar.component';
 
 @Directive({
@@ -9,5 +10,16 @@ export class BsExpandButtonDirective {
   constructor(navbar: BsNavbarComponent, templateRef: TemplateRef<any>) {
     navbar.expandButtonTemplate = templateRef;
   }
+  
+  public static ngTemplateContextGuard(
+    dir: BsExpandButtonDirective,
+    ctx: any
+  ): ctx is BsExpandButtonContext {
+    return true;
+  }
 
+}
+
+export class BsExpandButtonContext {
+  public $implicit: BsViewState = null!;
 }
