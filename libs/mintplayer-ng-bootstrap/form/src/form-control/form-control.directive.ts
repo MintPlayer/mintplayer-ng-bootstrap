46874ee12656b9a310cb1aeb2099c7c377415fea
Pieterjan <pieterjandeclippel@msn.com>
2023-09-18 17:22:41 +0200

    Only apply directive if inside a form (#149)
    * Only apply directive if inside a form
    
    * Added question mark

--- libs/mintplayer-ng-bootstrap/form/src/form-control/form-control.directive.ts
+++ libs/mintplayer-ng-bootstrap/form/src/form-control/form-control.directive.ts
@@ -1,8 +1,12 @@
-import { Directive, HostBinding } from '@angular/core';
+import { Directive, HostBinding, Optional } from '@angular/core';
+import { BsFormComponent } from '../form/form.component';
 
 @Directive({
   selector: 'bs-form input:not(.no-form-control), bs-form textarea:not(.no-form-control)'
 })
 export class BsFormControlDirective {
-  @HostBinding('class.form-control') formControlClass = true;
+  constructor(@Optional() form?: BsFormComponent) {
+    this.formControlClass = !!form;
+  }
+  @HostBinding('class.form-control') formControlClass: boolean;
 }
