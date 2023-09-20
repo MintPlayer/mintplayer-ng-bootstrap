9abce716686a49bdb2b79c961c2713d240ee9aef
Pieterjan De Clippel <pieterjandeclippel@msn.com>
2023-09-20 15:37:49 +0200

    Add more support for "let ... of ..."

--- libs/mintplayer-ng-bootstrap/select2/src/directive/suggestion-template/suggestion-template.directive.ts
+++ libs/mintplayer-ng-bootstrap/select2/src/directive/suggestion-template/suggestion-template.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, TemplateRef } from '@angular/core';
+import { Directive, Input, TemplateRef } from '@angular/core';
 import { BsSelect2Component } from '../../component/select2.component';
 import { HasId } from '@mintplayer/ng-bootstrap/has-id';
 
@@ -7,15 +7,19 @@ import { HasId } from '@mintplayer/ng-bootstrap/has-id';
 })
 export class BsSuggestionTemplateDirective<T extends HasId<U>, U> {
   constructor(private select2component: BsSelect2Component<T, U>, templateRef: TemplateRef<T>) {
-    this.select2component.itemTemplate = templateRef;
+    this.select2component.suggestionTemplate = templateRef;
   }
 
   public static ngTemplateContextGuard<T extends HasId<U>, U>(dir: BsSuggestionTemplateDirective<T, U>, ctx: any): ctx is BsSuggestionTemplateContext<Exclude<T, false | 0 | '' | null | undefined>, U> {
     return true;
   }
+
+  @Input() set bsSuggestionTemplateOf(value: T[]) {
+    this.select2component.suggestions = value;
+  }
 }
 
 export class BsSuggestionTemplateContext<T extends HasId<U>, U> {
-  item: T = null!;
+  $implicit: T = null!;
   select2: BsSelect2Component<T, U> = null!;
 }
