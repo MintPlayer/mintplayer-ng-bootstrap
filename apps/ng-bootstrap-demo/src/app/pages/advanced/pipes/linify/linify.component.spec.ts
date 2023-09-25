d9947358c9718a0a9ccc594bbf1c30a2ba0de5ae
Pieterjan De Clippel <pieterjandeclippel@msn.com>
2023-09-25 23:00:45 +0200

    Fix unit tests

--- apps/ng-bootstrap-demo/src/app/pages/advanced/pipes/linify/linify.component.spec.ts
+++ apps/ng-bootstrap-demo/src/app/pages/advanced/pipes/linify/linify.component.spec.ts
@@ -1,4 +1,12 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
+import { BsFormModule } from '@mintplayer/ng-bootstrap/form';
+import { BsGridModule } from '@mintplayer/ng-bootstrap/grid';
+import { BsLinifyModule } from '@mintplayer/ng-bootstrap/linify';
+import { BsListGroupModule } from '@mintplayer/ng-bootstrap/list-group';
+import { BsButtonTypeModule } from '@mintplayer/ng-bootstrap/button-type';
+import { BsToggleButtonModule } from '@mintplayer/ng-bootstrap/toggle-button';
+import { MockModule } from 'ng-mocks';
 
 import { LinifyComponent } from './linify.component';
 
@@ -8,6 +16,15 @@ describe('LinifyComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [
+        FormsModule,
+        MockModule(BsFormModule),
+        MockModule(BsGridModule),
+        MockModule(BsLinifyModule),
+        MockModule(BsListGroupModule),
+        MockModule(BsButtonTypeModule),
+        MockModule(BsToggleButtonModule),
+      ],
       declarations: [LinifyComponent]
     });
     fixture = TestBed.createComponent(LinifyComponent);
--- apps/ng-bootstrap-demo/src/app/pages/advanced/pipes/linify/linify.component.ts
+++ apps/ng-bootstrap-demo/src/app/pages/advanced/pipes/linify/linify.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import dedent from 'dedent';
+import * as dedent from 'dedent';
 
 @Component({
   selector: 'demo-linify',
@@ -11,7 +11,7 @@ export class LinifyComponent {
     See the stone set in your eyes
     See the thorn twist in your side
     I'll wait for you
-    
+
     Sleight of hand and twist of fate
     On a bed of nails, she makes me wait
     And I wait without you`;
