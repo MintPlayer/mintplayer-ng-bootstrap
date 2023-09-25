a97b4e28f10213ef79c33e2ad5bfee0bf8f6c11e
Pieterjan De Clippel <pieterjandeclippel@msn.com>
2023-09-25 14:54:16 +0200

    Fix build

--- libs/mintplayer-ng-bootstrap/carousel/src/index.ts
+++ libs/mintplayer-ng-bootstrap/carousel/src/index.ts
@@ -1,3 +1,4 @@
 export * from './carousel/carousel.component';
 export * from './carousel-image/carousel-image.directive';
+export * from './carousel-img/carousel-img.directive';
 export * from './carousel.module';
