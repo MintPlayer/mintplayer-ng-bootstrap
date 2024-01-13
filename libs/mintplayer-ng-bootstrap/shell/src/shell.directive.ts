db447191d86a9b10d05e699b29b1c4ae821c946f
Pieterjan De Clippel <pieterjandeclippel@msn.com>
2024-01-13 15:37:47 +0100

    Basic shell

--- libs/mintplayer-ng-bootstrap/shell/src/shell.directive.ts
+++ libs/mintplayer-ng-bootstrap/shell/src/shell.directive.ts
@@ -1,8 +1,7 @@
 import { Overlay } from '@angular/cdk/overlay';
-import { AnimationBuilder, state, transition, style, animate, AnimationPlayer, AnimationFactory } from '@angular/animations';
 import { TemplatePortal } from '@angular/cdk/portal';
 import { DOCUMENT } from '@angular/common';
-import { AfterViewInit, Directive, HostBinding, Inject, Input, TemplateRef, ViewContainerRef } from '@angular/core';
+import { AfterViewInit, Directive, ElementRef, Inject, Input, Renderer2, RendererFactory2, TemplateRef, ViewContainerRef } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 import { BsShellState } from './shell-state';
 
@@ -10,61 +9,41 @@ import { BsShellState } from './shell-state';
   selector: '[bsShell]',
 })
 export class BsShellDirective implements AfterViewInit {
-  constructor(private template: TemplateRef<any>, private overlay: Overlay, private vcRef: ViewContainerRef, @Inject(DOCUMENT) doc: any, private animationBuilder: AnimationBuilder) {
-    const bodyElement = (<Document>doc).querySelector<HTMLBodyElement>('html > body');
-    if (!bodyElement) {
-      throw 'No body element found';
+  constructor(private template: TemplateRef<any>, private overlay: Overlay, private vcRef: ViewContainerRef, @Inject(DOCUMENT) doc: any, private element: ElementRef, rendererFactory: RendererFactory2) {
+    const docu = <Document>doc;
+    this.renderer = rendererFactory.createRenderer(element.nativeElement, null);
+    if (!BsShellDirective.bodyElement) {
+      const bodyElement = (<Document>doc).querySelector<HTMLBodyElement>('html > body');
+      if (!bodyElement) {
+        throw 'No body element found';
+      }
+      BsShellDirective.bodyElement = bodyElement;
     }
-
-    this.bodyElement = bodyElement;
-    this.openBodyAnimation = this.animationBuilder.build([
-      style({ 'margin-left': '0' }),
-      animate(this.duration, style({ 'margin-left': '300px' })),
-    ]);
-    this.closeBodyAnimation = this.animationBuilder.build([
-      style({ 'margin-left': '300px' }),
-      animate(this.duration, style({ 'margin-left': '0' })),
-    ]);
-    this.openSidebarAnimation = this.animationBuilder.build([
-      style({ transform: 'translateX(-100%)' }),
-      animate(this.duration, style({ transform: 'translateX(0)' })),
-    ]);
-    this.closeSidebarAnimation = this.animationBuilder.build([
-      style({ transform: 'translateX(0)' }),
-      animate(this.duration, style({ transform: 'translateX(-100%)' })),
-    ]);
+    if (!BsShellDirective.styleTag) {
+      const styleTag = BsShellDirective.styleTag = <HTMLStyleElement>this.renderer.createElement('style');
+      styleTag.innerHTML = `.bs-shell-sidebar{transform:translateX(-100%);transition:transform linear 300ms;top:0}.bs-shell-sidebar.show{transform:translateX(0)}html>body{margin-left:0;transition:margin-left linear 300ms}html>body.sidebar-show{margin-left:${this.size}px}`;
+      this.renderer.appendChild(docu.head, styleTag);
+    }
   }
 
-  bodyElement: HTMLBodyElement;
+  size = 300;
+  renderer: Renderer2;
+  static bodyElement: HTMLBodyElement;
   sidebarElement?: HTMLElement;
-  openBodyAnimation: AnimationFactory;
-  closeBodyAnimation: AnimationFactory;
-  openSidebarAnimation: AnimationFactory;
-  closeSidebarAnimation: AnimationFactory;
   shellState$ = new BehaviorSubject<BsShellState>('auto');
   @Input('bsShell') public set shellState(value: BsShellState) {
     this.shellState$.next(value);
     switch (value) {
       case 'show':
         if (this.sidebarElement) {
-          const openBodyPlayer = this.openBodyAnimation.create(this.bodyElement);
-          const openSidebarPlayer = this.openSidebarAnimation.create(this.sidebarElement);
-          openSidebarPlayer.onDone(() => {
-
-          });
-          openSidebarPlayer.play();
-          openBodyPlayer.play();
+          this.renderer.addClass(this.sidebarElement, 'show');
+          this.renderer.addClass(BsShellDirective.bodyElement, 'sidebar-show');
         }
         break;
       case 'hide':
         if (this.sidebarElement) {
-          const closeBodyPlayer = this.closeBodyAnimation.create(this.bodyElement);
-          const closeSidebarPlayer = this.closeSidebarAnimation.create(this.sidebarElement);
-          closeSidebarPlayer.onDone(() => {
-
-          });
-          closeSidebarPlayer.play();
-          closeBodyPlayer.play();
+          this.renderer.removeClass(this.sidebarElement, 'show');
+          this.renderer.removeClass(BsShellDirective.bodyElement, 'sidebar-show');
         }
         break;
       default:
@@ -73,6 +52,7 @@ export class BsShellDirective implements AfterViewInit {
   }
 
   duration = 300;
+  static styleTag?: HTMLStyleElement;
 
   ngAfterViewInit() {
     const overlayRef = this.overlay.create({
@@ -84,9 +64,7 @@ export class BsShellDirective implements AfterViewInit {
     const viewRef = overlayRef.attach(portal);
     this.sidebarElement = viewRef.rootNodes[0];
     if (this.sidebarElement) {
-      this.sidebarElement.classList.add('position-absolute');
-      this.sidebarElement.style.top = '0';
-      this.sidebarElement.style.transform = 'translateX(-100%)';
+      this.sidebarElement.classList.add('position-absolute', 'h-100', 'bs-shell-sidebar');
     }
   }
 }
