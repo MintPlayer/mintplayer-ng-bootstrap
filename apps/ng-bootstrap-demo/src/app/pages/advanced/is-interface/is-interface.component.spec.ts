3468fb7bdfbc71c2c0c8e2e09b9f559feb1780ab
Pieterjan De Clippel <pieterjandeclippel@msn.com>
2024-04-01 22:10:53 +0200

    Fix test

--- apps/ng-bootstrap-demo/src/app/pages/advanced/is-interface/is-interface.component.spec.ts
+++ apps/ng-bootstrap-demo/src/app/pages/advanced/is-interface/is-interface.component.spec.ts
@@ -8,7 +8,7 @@ describe('IsInterfaceComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [IsInterfaceComponent]
+      imports: [IsInterfaceComponent]
     })
     .compileComponents();
     
