e839eb6734cb0e211f7bdd7e3c4a5ba1644500ad
Pieterjan De Clippel <pieterjandeclippel@msn.com>
2024-04-03 22:14:00 +0200

    Fixed other test

--- libs/mintplayer-ng-bootstrap/multiselect/src/component/multiselect.component.spec.ts
+++ libs/mintplayer-ng-bootstrap/multiselect/src/component/multiselect.component.spec.ts
@@ -1,7 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { BsButtonTypeDirective } from '@mintplayer/ng-bootstrap/button-type';
 import { BsDropdownModule } from '@mintplayer/ng-bootstrap/dropdown';
-import { MockModule } from 'ng-mocks';
+import { MockDirective, MockModule } from 'ng-mocks';
 import { BsMultiselectComponent } from './multiselect.component';
 
 describe('BsMultiselectComponent', () => {
@@ -12,7 +12,7 @@ describe('BsMultiselectComponent', () => {
     await TestBed.configureTestingModule({
       imports: [
         MockModule(BsDropdownModule),
-        MockModule(BsButtonTypeDirective),
+        MockDirective(BsButtonTypeDirective),
       ],
       declarations: [
         // Unit to test
