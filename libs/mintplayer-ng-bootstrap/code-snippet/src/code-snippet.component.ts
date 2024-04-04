f4c2bc7cf0aadb1f10af984befa4a202ecbcee70
Pieterjan De Clippel <pieterjandeclippel@msn.com>
2024-04-04 08:56:13 +0200

    Code-snippet show language

--- libs/mintplayer-ng-bootstrap/code-snippet/src/code-snippet.component.ts
+++ libs/mintplayer-ng-bootstrap/code-snippet/src/code-snippet.component.ts
@@ -1,26 +1,41 @@
-import { Component, Input, TemplateRef, ViewChild } from '@angular/core';
+import { AsyncPipe } from '@angular/common';
+import { Component, EventEmitter, Input, Output, TemplateRef, ViewChild } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { BsCopyDirective } from '@mintplayer/ng-bootstrap/copy';
 import { BsOffcanvasModule } from '@mintplayer/ng-bootstrap/offcanvas';
-import { HighlightModule } from 'ngx-highlightjs';
+import { HighlightAutoResult, HighlightModule } from 'ngx-highlightjs';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'bs-code-snippet',
   templateUrl: './code-snippet.component.html',
   styleUrls: ['./code-snippet.component.scss'],
   standalone: true,
-  imports: [BsCopyDirective, BsOffcanvasModule, HighlightModule]
+  imports: [AsyncPipe, BsCopyDirective, BsOffcanvasModule, HighlightModule]
 })
 export class BsCodeSnippetComponent {
+
+  constructor() {
+    this.language$.pipe(takeUntilDestroyed())
+      .subscribe((language) => this.detectedLanguage.emit(language));
+  }
 
   offcanvasVisible = false;
   @Input() public codeToCopy = '';
   @Input() public languages: string[] | null = null;
   @Input() public lineNumbers = false;
   @ViewChild('copiedTemplate') copiedTemplate!: TemplateRef<any>;
+  @Output() public detectedLanguage = new EventEmitter<string>();
+
+  language$ = new BehaviorSubject<string>('code');
 
   copiedHtml() {
     this.offcanvasVisible = true;
     setTimeout(() => this.offcanvasVisible = false, 3000);
+  }
+
+  onHighlighted(result: HighlightAutoResult) {
+    this.language$.next(result.language ?? 'code');
   }
 
 }
