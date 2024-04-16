ad4ca750cf1f9e2e7c646a2886703c7119f041fa
Pieterjan De Clippel <pieterjandeclippel@msn.com>
2024-04-16 22:42:57 +0200

    Accordion

--- apps/ng-bootstrap-demo/src/app/pages/overlay/shell/shell.component.ts
+++ apps/ng-bootstrap-demo/src/app/pages/overlay/shell/shell.component.ts
@@ -1,6 +1,7 @@
 import { Component, ViewChild } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Color } from '@mintplayer/ng-bootstrap';
+import { BsAccordionModule } from '@mintplayer/ng-bootstrap/accordion';
 import { BsButtonGroupComponent } from '@mintplayer/ng-bootstrap/button-group';
 import { BsButtonTypeDirective } from '@mintplayer/ng-bootstrap/button-type';
 import { BsShellComponent, BsShellModule } from '@mintplayer/ng-bootstrap/shell';
@@ -9,7 +10,7 @@ import { BsToggleButtonModule } from '@mintplayer/ng-bootstrap/toggle-button';
 @Component({
   selector: 'demo-shell',
   standalone: true,
-  imports: [FormsModule, BsShellModule, BsButtonGroupComponent, BsButtonTypeDirective, BsToggleButtonModule],
+  imports: [FormsModule, BsShellModule, BsAccordionModule, BsButtonGroupComponent, BsButtonTypeDirective, BsToggleButtonModule],
   templateUrl: './shell.component.html',
   styleUrl: './shell.component.scss'
 })
