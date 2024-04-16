91f519cac17809d52338c0b4818c7314984f2a15
Pieterjan De Clippel <pieterjandeclippel@msn.com>
2024-04-16 22:13:47 +0200

    Show/hide sidebar

--- apps/ng-bootstrap-demo/src/app/pages/overlay/shell/shell.component.ts
+++ apps/ng-bootstrap-demo/src/app/pages/overlay/shell/shell.component.ts
@@ -1,17 +1,21 @@
 import { Component, ViewChild } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { Color } from '@mintplayer/ng-bootstrap';
+import { BsButtonGroupComponent } from '@mintplayer/ng-bootstrap/button-group';
 import { BsButtonTypeDirective } from '@mintplayer/ng-bootstrap/button-type';
 import { BsShellComponent, BsShellModule } from '@mintplayer/ng-bootstrap/shell';
+import { BsToggleButtonModule } from '@mintplayer/ng-bootstrap/toggle-button';
 
 @Component({
   selector: 'demo-shell',
   standalone: true,
-  imports: [BsShellModule, BsButtonTypeDirective],
+  imports: [FormsModule, BsShellModule, BsButtonGroupComponent, BsButtonTypeDirective, BsToggleButtonModule],
   templateUrl: './shell.component.html',
   styleUrl: './shell.component.scss'
 })
 export class ShellComponent {
   colors = Color;
+  shellState: 'auto' | 'show' | 'hide' = 'auto';
   @ViewChild('shell') shell!: BsShellComponent;
 
   setSize(rem: number) {
--- libs/mintplayer-ng-bootstrap/shell/src/shell/shell.component.ts
+++ libs/mintplayer-ng-bootstrap/shell/src/shell/shell.component.ts
@@ -1,4 +1,6 @@
-import { Component, ElementRef, TemplateRef, ViewChild } from '@angular/core';
+import { Component, ElementRef, Input, TemplateRef, ViewChild } from '@angular/core';
+import { BehaviorSubject, Observable, map } from 'rxjs';
+import { BsShellState } from '../shell-state';
 
 @Component({
   selector: 'bs-shell',
@@ -6,8 +8,22 @@ import { Component, ElementRef, TemplateRef, ViewChild } from '@angular/core';
   styleUrl: './shell.component.scss'
 })
 export class BsShellComponent {
+  constructor() {
+    this.stateClass$ = this.state$.pipe(map((state) => {
+      if (state === 'auto') return null;
+      else return state;
+    }));
+  }
+
   sidebarTemplate: TemplateRef<any> | null = null;
   @ViewChild('root') rootElement!: ElementRef<HTMLDivElement>;
+
+  @Input() set state(value: BsShellState) {
+    this.state$.next(value);
+  }
+
+  state$ = new BehaviorSubject<BsShellState>('auto');
+  stateClass$: Observable<string | null>;
 
   public setSize(size: string) {
     this.rootElement.nativeElement.style.setProperty('--size', size);
