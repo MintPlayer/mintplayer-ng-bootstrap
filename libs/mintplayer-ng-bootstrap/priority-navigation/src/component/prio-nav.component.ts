3cc6a04079a8507587a8443d8eb175911dfe173d
Pieterjan De Clippel <pieterjandeclippel@msn.com>
2024-05-01 17:53:31 +0200

    Success

--- libs/mintplayer-ng-bootstrap/priority-navigation/src/component/prio-nav.component.ts
+++ libs/mintplayer-ng-bootstrap/priority-navigation/src/component/prio-nav.component.ts
@@ -1,7 +1,7 @@
 import { Component, ContentChildren, ElementRef, HostBinding, QueryList, TemplateRef, ViewChildren } from '@angular/core';
 import { BsObserveSizeDirective } from '@mintplayer/ng-bootstrap/observe-size';
 import { BsPrioNavElementDirective } from '../prio-nav-element.directive';
-import { BehaviorSubject, Observable, combineLatest, from, map, of, reduce, switchMap } from 'rxjs';
+import { BehaviorSubject, Observable, combineLatest, debounce, debounceTime, from, map, of, reduce, switchMap } from 'rxjs';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
@@ -12,17 +12,31 @@ import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 })
 export class BsPrioNavComponent {
   constructor(private observer: BsObserveSizeDirective) {
-    const obs$ = this.observers$
+    const rightBounds$ = this.observers$
       .pipe(map(observers => observers.map((_, index) => observers.slice(0, index + 1))))
       .pipe(switchMap(o => o))
-      .pipe(map(o => o.map(dir => dir.width$)))
+      .pipe(map(o => o.map(dir => dir.width$)), debounceTime(10))
       .pipe(switchMap(o => combineLatest(o)))
       .pipe(map(w => w.map(x => x || 0)))
-      .pipe(map(w => w.reduce((acc: number[], curr: number) => [...acc, acc.reduce((s, c) => s + c, 0) + curr], [])));
+      .pipe(map((w) => w.map((n, i) => w.slice(0, i + 1).reduce((a,b) => a + b, 0))));
 
-    obs$.pipe(takeUntilDestroyed()).subscribe((x) => {
+    const show$ = combineLatest([rightBounds$, observer.width$])
+      .pipe(map(([el, w]) => {
+        console.warn({el, w});
+        return el.map(right => {
+          if (w) {
+            return right <= w;
+          } else {
+            return true;
+          }
+        });
+      }));
+
+      
+    show$.pipe(takeUntilDestroyed()).subscribe((x) => {
       console.log(x);
     });
+
   }
 
   @HostBinding('class.d-block')
