c62224bdf772d588789128c27ba7e6bef47f3d6e
Pieterjan <pieterjandeclippel@msn.com>
2024-05-05 10:19:35 +0200

    New grid (#198)
    * New grid directive
    
    * Fix code
    
    * Bump version

--- libs/mintplayer-ng-bootstrap/grid/src/directives/column/column.directive.ts
+++ libs/mintplayer-ng-bootstrap/grid/src/directives/column/column.directive.ts
@@ -1,32 +1,29 @@
 import { Directive, HostBinding, Input, DestroyRef } from '@angular/core';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
-import { BehaviorSubject, map } from 'rxjs';
-import { BsColumnDefinition } from '../../interfaces/column-definition';
+import { BehaviorSubject, combineLatest, map } from 'rxjs';
 
 @Directive({
-  selector: '[bsColumn]'
+  selector: '[xxs],[xs],[sm],[md],[lg],[xl],[xxl]'
 })
 export class BsGridColumnDirective {
   constructor(destroy: DestroyRef) {
-    this.customColClasses$
-      .pipe(map((data) => {
-        if (!data) {
-          return 'col';
-        } else {
-          return Object.keys(data)
-            .map(key => ({
-              key,
-              value: (<any>data)[key]
-            }))
-            .map(v => {
-              if (v.key === '_') {
-                return `col-${v.value}`;
-              } else {
-                return `col-${v.key}-${v.value}`;
-              }
-            })
-            .join(' ');
-        }
+    combineLatest([this.xxs$, this.xs$, this.sm$, this.md$, this.lg$, this.xl$, this.xxl$])
+      .pipe(map(([xxs, xs, sm, md, lg, xl, xxl]) => ({ xxs, xs, sm, md, lg, xl, xxl })))
+      .pipe(map((sizes) => {
+        return Object.keys(sizes)
+          .map(key => ({
+            key,
+            value: (<any>sizes)[key],
+          }))
+          .filter(v => v.value && (v.key !== 'xxs'))
+          .map(v => {
+            switch (v.key) {
+              case '': return 'col';
+              case 'xxs': return `col-${v.value}`;
+              default: return `col-${v.key}-${v.value}`;
+            }
+          })
+          .join(' ');
       }))
       .pipe(takeUntilDestroyed(destroy))
       .subscribe((classList) => {
@@ -34,10 +31,44 @@ export class BsGridColumnDirective {
       });
   }
 
-  private customColClasses$ = new BehaviorSubject<BsColumnDefinition | '' | undefined>(undefined);
+  // private customColClasses$ = new BehaviorSubject<BsColumnDefinition | '' | undefined>(undefined);
   @HostBinding('class') classList: string | null = null
 
-  @Input() public set bsColumn(value: BsColumnDefinition | '' | undefined) {
-    this.customColClasses$.next(value);
+  xxs$ = new BehaviorSubject<number | undefined>(undefined);
+  xs$ = new BehaviorSubject<number | undefined>(undefined);
+  sm$ = new BehaviorSubject<number | undefined>(undefined);
+  md$ = new BehaviorSubject<number | undefined>(undefined);
+  lg$ = new BehaviorSubject<number | undefined>(undefined);
+  xl$ = new BehaviorSubject<number | undefined>(undefined);
+  xxl$ = new BehaviorSubject<number | undefined>(undefined);
+
+  @Input() public set xxs(value: number | undefined) {
+    this.xxs$.next(value);
   }
+  @Input() public set xs(value: number | undefined) {
+    this.xs$.next(value);
+  }
+  @Input() public set sm(value: number | undefined) {
+    this.sm$.next(value);
+  }
+  @Input() public set md(value: number | undefined) {
+    this.md$.next(value);
+  }
+  @Input() public set lg(value: number | undefined) {
+    this.lg$.next(value);
+  }
+  @Input() public set xl(value: number | undefined) {
+    this.xl$.next(value);
+  }
+  @Input() public set xxl(value: number | undefined) {
+    this.xxl$.next(value);
+  }
+}
+
+@Directive({
+  selector: '[col]'
+})
+export class BsGridColDirective {
+  @HostBinding('class.col') colClass = true;
+  @Input() set col(value: undefined) {}
 }
--- libs/mintplayer-ng-bootstrap/grid/src/grid.module.ts
+++ libs/mintplayer-ng-bootstrap/grid/src/grid.module.ts
@@ -3,12 +3,12 @@ import { CommonModule } from '@angular/common';
 import { BsContainerComponent } from '@mintplayer/ng-bootstrap/container';
 import { BsGridComponent } from './component/grid.component';
 import { BsGridRowDirective } from './directives/row/row.directive';
-import { BsGridColumnDirective } from './directives/column/column.directive';
+import { BsGridColDirective, BsGridColumnDirective } from './directives/column/column.directive';
 import { BsColFormLabelDirective } from './directives/col-form-label/col-form-label.directive';
 
 @NgModule({
-  declarations: [BsGridComponent, BsGridRowDirective, BsGridColumnDirective, BsColFormLabelDirective],
+  declarations: [BsGridComponent, BsGridRowDirective, BsGridColumnDirective, BsGridColDirective, BsColFormLabelDirective],
   imports: [CommonModule, BsContainerComponent],
-  exports: [BsGridComponent, BsGridRowDirective, BsGridColumnDirective, BsColFormLabelDirective],
+  exports: [BsGridComponent, BsGridRowDirective, BsGridColumnDirective, BsGridColDirective, BsColFormLabelDirective],
 })
 export class BsGridModule {}
