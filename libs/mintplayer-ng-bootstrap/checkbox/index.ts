5fada39ac793f92581f6c3a1305cf53f95a12aeb
Pieterjan De Clippel <pieterjandeclippel@msn.com>
2024-07-07 02:50:22 +0200

    expose class

--- libs/mintplayer-ng-bootstrap/checkbox/index.ts
+++ libs/mintplayer-ng-bootstrap/checkbox/index.ts
@@ -1,3 +1,4 @@
 export * from './src/component/checkbox.component';
 export * from './src/directives/checkbox-group/checkbox-group.directive';
+export * from './src/directives/value-accessor/checkbox-group.value-accessor.directive';
 export * from './src/checkbox.module';
