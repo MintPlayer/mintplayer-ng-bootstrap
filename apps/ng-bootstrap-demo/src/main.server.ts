b5b5bddb55628032b8c1b4638e7c6f8c56adb742
PieterjanDeClippel <pieterjandeclippel@msn.com>
2025-08-12 15:14:30 +0200

    Kinda fixed the build

--- apps/ng-bootstrap-demo/src/main.server.ts
+++ apps/ng-bootstrap-demo/src/main.server.ts
@@ -1,6 +1,7 @@
 import { bootstrapApplication } from "@angular/platform-browser";
-import { renderApplication } from "@angular/platform-server";
 import { AppComponent } from "./app/app.component";
 import { config } from './app/app.config';
 
-renderApplication(() => bootstrapApplication(AppComponent, config), {});
+const bootstrap = () => bootstrapApplication(AppComponent, config);
+
+export default bootstrap;
