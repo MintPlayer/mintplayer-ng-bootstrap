e340b2f66bd7e1c0b99871fd7eedb90a230567e4
PieterjanDeClippel <pieterjandeclippel@msn.com>
2025-11-23 21:17:17 +0100

    Tab control

--- apps/ng-bootstrap-demo/src/app/pages/basic/containers/tab-control/tab-control.component.ts
+++ apps/ng-bootstrap-demo/src/app/pages/basic/containers/tab-control/tab-control.component.ts
@@ -3,14 +3,14 @@ import { FormsModule } from '@angular/forms';
 import { BsForDirective } from '@mintplayer/ng-bootstrap/for';
 import { BsGridColDirective, BsGridComponent, BsGridRowDirective } from '@mintplayer/ng-bootstrap/grid';
 import { BsSelectComponent, BsSelectOption } from '@mintplayer/ng-bootstrap/select';
-import { BsTabControlModule, BsTabsPosition } from '@mintplayer/ng-bootstrap/tab-control';
+import { BsTabControlComponent, BsTabPageComponent, BsTabPageHeaderDirective, BsTabsPosition } from '@mintplayer/ng-bootstrap/tab-control';
 import { BsToggleButtonComponent } from '@mintplayer/ng-bootstrap/toggle-button';
 
 @Component({
   selector: 'demo-tab-control',
   templateUrl: './tab-control.component.html',
   styleUrls: ['./tab-control.component.scss'],
-  imports: [FormsModule, BsForDirective, BsGridComponent, BsGridRowDirective, BsGridColDirective, BsSelectComponent, BsSelectOption, BsTabControlModule, BsToggleButtonComponent]
+  imports: [FormsModule, BsForDirective, BsGridComponent, BsGridRowDirective, BsGridColDirective, BsSelectComponent, BsSelectOption, BsTabControlComponent, BsTabPageComponent, BsTabPageHeaderDirective, BsToggleButtonComponent]
 })
 export class TabControlComponent {
   tabsPosition: BsTabsPosition = 'top';
--- libs/mintplayer-ng-bootstrap/tab-control/src/tab-page-header/tab-page-header.directive.ts
+++ libs/mintplayer-ng-bootstrap/tab-control/src/tab-page-header/tab-page-header.directive.ts
@@ -2,7 +2,6 @@ import { Directive, TemplateRef } from '@angular/core';
 
 @Directive({
   selector: '[bsTabPageHeader]',
-  standalone: false,
 })
 export class BsTabPageHeaderDirective {
   constructor(public template: TemplateRef<any>) {}
