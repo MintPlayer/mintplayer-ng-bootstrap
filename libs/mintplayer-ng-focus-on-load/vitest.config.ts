0be7a7f32297efc00a7c2ebb36f2ab9c57e328fc
PieterjanDeClippel <pieterjandeclippel@msn.com>
2025-11-29 02:12:34 +0100

    Fixed unit tests

--- libs/mintplayer-ng-focus-on-load/vitest.config.ts
+++ libs/mintplayer-ng-focus-on-load/vitest.config.ts
@@ -1,5 +1,4 @@
-/// <reference types="vitest" />
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 import angular from '@analogjs/vite-plugin-angular';
 import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
 
